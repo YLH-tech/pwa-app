f899196811bd6d7a2f7d25718b9b8e8674eb6cd0
YLH-tech <yelinhtet0101@gmail.com>
2024-10-18 15:51:38 +0630

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,28 @@
+const cacheName = 'pwa-app-v1';
+const assetsToCache = [
+    '/pwa-app/',
+    '/pwa-app/index.html',
+    '/pwa-app/manifest.json',
+    '/pwa-app/images/icon-192x192.png',
+    '/pwa-app/images/icon-512x512.png'
+];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('pwa-cache').then((cache) => {
-      return cache.addAll([
-        '/pwa-app/index.html',
-        '/pwa-app/style.css',
-        '/images/icon-192x192.png',
-        '/images/icon-512x512.png'
-      ]);
-    })
-  );
+// Install the service worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(cache => {
+            return cache.addAll(assetsToCache);
+        })
+    );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+// Fetch the cached assets
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
